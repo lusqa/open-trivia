43ea55f5a53b91076f55c27b09218d398a786078
Lucas de Oliveira Gomes <lucasdeoliveira887@gmail.com>
2019-10-10 20:25:22 -0300

    Added a header component and removed unnecessary code

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <span class="header__label">{{ label }}</span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$primary: #343c58;
+
+.header {
+  display: flex;
+  min-height: 56px;
+  background-color: $primary;
+}
+.header__label {
+  font-family: "Avenir", sans-serif;
+  font-size: 18px;
+  line-height: 24px;
+  color: #ffffff;
+  padding: 16px 26px;
+}
+</style>
