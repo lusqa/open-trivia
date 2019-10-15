719483589194d05c86b2c2d4a2899c0effc4b327
Lucas de Oliveira Gomes <lucasdeoliveira887@gmail.com>
2019-10-15 20:54:57 -0300

    Added a frameork of the Card component

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,12 @@ export default {
 </script>
 
 <style lang="scss">
+html,
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
 #app {
   font-family: "Avenir", sans-serif;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,55 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1 class="page-title">Categorias</h1>
+    <div class="card-manager">
+      <Card id="card" label="História" />
+      <Card id="card" label="Geografia" />
+      <Card id="card" label="Mitologia" />
+      <Card id="card" label="Esportes" />
+      <Card id="card" label="Política" />
+      <Card id="card" label="Conhecimentos Gerais" />
+    </div>
+  </div>
 </template>
 
 <script>
+import Card from "@/components/Card";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    Card
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/stylesheets/colors.scss";
+
+.home {
+  padding: 16px;
+}
+
+.page-title {
+  color: $ink;
+}
+
+.card-manager {
+  .card {
+    @media screen and (max-width: 992px) {
+      width: 210px;
+    }
+
+    @media screen and (max-width: 768px) {
+      width: 158px;
+    }
+
+    @media screen and (max-width: 576px) {
+      width: 165px;
+    }
+
+    @media screen and (max-width: 375px) {
+      width: 163px;
+    }
+  }
+}
+</style>
