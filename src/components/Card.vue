4d6fb408897c69763c322ee170afb8933d2d9dd1
Lucas de Oliveira Gomes <lucasdeoliveira887@gmail.com>
2019-10-15 22:08:58 -0300

    Added a card component to be used on the categories page

---
+++ src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <span class="card__label">{{ label }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeedbackDialog",
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/stylesheets/colors.scss";
+
+$bg-color: #0467db;
+
+.card {
+  display: flex;
+  width: 239px;
+  height: 104px;
+  border-radius: 8px;
+  border: 1px solid #e4e4e6;
+  align-items: flex-end;
+  margin: 16px 0;
+
+  &__label {
+    font-size: 18px;
+    color: $ink;
+    padding: 16px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,7 @@
 
 <script>
 import Card from "@/components/Card";
+
 export default {
   name: "Home",
   components: {
